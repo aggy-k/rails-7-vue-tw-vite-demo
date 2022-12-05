af715aeb6e87d6c953096189b18a10eec525e998
Agnes Selvita <a.selvita@gmail.com>
2022-12-05 14:41:10 +0800

    Testing Rails 7 with Vue sprinkles setup with vite

--- app/frontend/components/views/Home.vue
+++ app/frontend/components/views/Home.vue
@@ -1,4 +1,19 @@
 <template>
-  <h3>This is the homepage component</h3>
-   <h6>You can find me in: <span class="px-2 py-1 bg-slate-100">frontend/components/views/Home.vue</span></h6>
+  <div>
+    <h3>This is the homepage component</h3>
+    <h6>You can find me in: <span class="px-2 py-1 bg-slate-100">frontend/components/views/Home.vue</span></h6>
+
+    <div class="mt-8">
+      <button class="btn btn-primary">Test Button</button>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    name: 'Home',
+    mounted() {
+      console.log('hello from HOME')
+    }
+  }
+</script>
--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,9 +1,26 @@
 import "./main.scss";
 
-import { createApp } from 'vue';
+// Setup vue as below to use Vue as SPA
+// import { createApp } from 'vue';
+// import Home from "../components/views/Home.vue";
+// if (document.querySelector('#home')) {
+//   const home = createApp(Home);
+//   home.mount('#home');
+// }
+
+
+// Setup vue as below to sprinkle Vue components on rails views
+import { createApp } from 'vue/dist/vue.esm-bundler';
 import Home from "../components/views/Home.vue";
 
-if (document.querySelector('#home')) {
-  const home = createApp(Home);
-  home.mount('#home');
-}
+const app = createApp({
+  data() {
+    return {
+      message: 'Hello Vue 3 and Rails'
+    }
+  }
+})
+
+// import (and use) the components one by one
+app.component('Home', Home);
+app.mount('[data-behavior="vue"]');
